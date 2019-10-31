ccd2905d00e9ca9a4bea42a289975614dbf382b0
eric-hochberger <erichochberger2020@u.northwestern.edu>
2019-10-31 01:06:09 -0500

    updated annotations and libraries

--- app.R
+++ app.R
@@ -1,33 +1,23 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Album Wordclouds Version 1.0.0 
+# Eric Hochberger 10/30/19
+
 
 library(shiny)
 library(rvest)
 library(tidyverse)
 library(janitor)
-library(rlist)
-library(rjson)
-library(tidyjson)
-library(jsonlite)
-library(dbConnect)
-library(tools)
 library(wordcloud)
 library(tm)
 library(RColorBrewer)
 
-# Define UI for application that draws a histogram
+# Define UI 
 ui <- fluidPage(
    
    # Application title
    titlePanel(h1("Album Word Cloud Generator")),
    
-   # Sidebar with a slider input for number of bins 
+
    sidebarLayout(
       sidebarPanel(
         textInput("text", label = (h3("Please paste the Genius.com album-page URL for your desired album below: ")), 
@@ -37,7 +27,6 @@ ui <- fluidPage(
       ),
       
       
-      # Show a plot of the generated distribution
         mainPanel(
           tabsetPanel(type = "tabs",
                       tabPanel("Cloud",
@@ -68,7 +57,7 @@ ui <- fluidPage(
    )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
    
   
@@ -97,7 +86,6 @@ server <- function(input, output) {
   
   lyrics <- reactive({
 
-    #songs <- album_songs()
     
     words <- ""
     for(i in album_songs() ) {
@@ -115,13 +103,15 @@ server <- function(input, output) {
           str_replace_all("\n|\\[.*?\\]|,|-|More on Genius|Lyrics", " ") %>% 
           str_replace_all("\'", "") %>% 
           str_replace_all(input$text %>% read_html() %>% html_node("a.header_with_cover_art-primary_info-primary_artist") %>% html_text(), " ") %>%
-          # str_replace_all('\\[.*?\\]', " ") %>% 
-          # str_replace_all(",", " ") %>% str_replace_all("-", " ") %>% 
           str_replace_all("([a-z])([A-Z])", "\\1 \\2") %>%
-          # str_replace_all("More on Genius"|"Lyrics", " ") %>% 
           append(words)
       )
     }
+    
+
+# Process album lyrics --------------------------------------
+
+    
     words <- Corpus(VectorSource(words))
     words <- tm_map(words, stripWhitespace)
     words <- tm_map(words, content_transformer(tolower))
@@ -129,16 +119,15 @@ server <- function(input, output) {
     words <- tm_map(words, removePunctuation)
     words
 
-    #Process Data
-
-    # lyrics <- Corpus(VectorSource(lyrics))
-    #
-    # lyrics <- tm_map(lyrics, content_transformer(tolower))
-    #
-    # lyrics <- tm_map(lyrics, removeWords, stopwords('english'))
 
 
   })
+  
+  
+
+# Wordcloud ---------------------------------------------------------------
+
+  
   
    output$Plot <- renderPlot({
    
@@ -146,14 +135,14 @@ server <- function(input, output) {
 
     lyrics() %>% wordcloud(colors = brewer.pal(8, "Dark2"), random.order = FALSE, rot.per = .25, max.words = 100, scale = c(5, 0.5))
      
-   #as.character(lyrics()) %>% wordcloud(colors = brewer.pal(8, "Dark2"), random.order = FALSE, rot.per = .25, max.words = 100, scale = c(5, 0.5))
+
      
-     
-     # sum_lyrics <- TermDocumentMatrix(lyrics) %>% as.matrix()
-     # 
-     # sort(rowSums(lyrics1), decreasing = TRUE) %>% enframe() %>% filter(row_number() <= 10) %>% ggplot(aes(name, value)) + geom_col() + theme_minimal()
+    
    })
    
+
+# Histogram ---------------------------------------------------------------
+
    
    output$sum_Plot <- renderPlot({
      
